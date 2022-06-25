90a11d85cb39175adb7447278b6445de3f5d675b
Juan Manuel Pajoy Lopez <jpajoyl@unal.edu.co>
2022-06-25 10:57:14 -0500

    intento 2

--- preguntas.py
+++ preguntas.py
@@ -50,7 +50,7 @@ def pregunta_02():
         y_pred = np.matmul(x_poly, params)
 
         # Calcule el error
-        error = y - y_pred
+        error = y_pred - y
 
         # Calcule el gradiente
         gradient = x_poly.T.dot(error)
